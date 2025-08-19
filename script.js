90afec5ae432caa659615317f0301c02c0bcaefb
Githubuser102234 <119143251+Githubuser102234@users.noreply.github.com>
2025-08-18 22:06:54 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,96 @@
+const calculator = document.querySelector('.calculator');
+const calculatorScreen = calculator.querySelector('.calculator-screen');
+const buttons = calculator.querySelector('.calculator-buttons');
+
+let displayValue = '';
+let firstOperand = null;
+let operator = null;
+let waitingForSecondOperand = false;
+
+function updateDisplay() {
+    calculatorScreen.value = displayValue;
+}
+
+buttons.addEventListener('click', (event) => {
+    const { target } = event;
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if (target.classList.contains('operator')) {
+        handleOperator(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('all-clear')) {
+        resetCalculator();
+        updateDisplay();
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
+});
+
+function inputDigit(digit) {
+    if (waitingForSecondOperand) {
+        displayValue = digit;
+        waitingForSecondOperand = false;
+    } else {
+        displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+}
+
+function inputDecimal(dot) {
+    if (waitingForSecondOperand) {
+        displayValue = '0.';
+        waitingForSecondOperand = false;
+        return;
+    }
+
+    if (!displayValue.includes(dot)) {
+        displayValue += dot;
+    }
+}
+
+function handleOperator(nextOperator) {
+    const inputValue = parseFloat(displayValue);
+
+    if (operator && waitingForSecondOperand) {
+        operator = nextOperator;
+        return;
+    }
+
+    if (firstOperand === null) {
+        firstOperand = inputValue;
+    } else if (operator) {
+        const result = performCalculation[operator](firstOperand, inputValue);
+        displayValue = `${parseFloat(result.toFixed(7))}`;
+        firstOperand = result;
+    }
+
+    waitingForSecondOperand = true;
+    operator = nextOperator;
+}
+
+const performCalculation = {
+    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+    '=': (firstOperand, secondOperand) => secondOperand,
+};
+
+function resetCalculator() {
+    displayValue = '';
+    firstOperand = null;
+    operator = null;
+    waitingForSecondOperand = false;
+}
